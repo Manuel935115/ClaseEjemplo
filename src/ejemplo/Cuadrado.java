244baffddaeabe67421c317d665106a327a18c7c
Martín Mateos <manuel.martin3@isepceu.es>
2025-06-10 17:15:48 +0200

    segunda clase

---
+++ src/ejemplo/Cuadrado.java
@@ -0,0 +1,56 @@
+package ejemplo;
+
+public class Cuadrado {
+
+	private String nombre;
+	private String color;
+	private int id;
+	private double lado;
+	
+	// constructor
+	public Cuadrado() {
+		
+	}
+	public Cuadrado(String nombre, String color, int id, double lado) {
+		this.nombre = nombre;
+		this.color = color;
+		this.id = id;
+		this.lado = lado;
+	}
+
+	// getters y setters
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getLado() {
+		return lado;
+	}
+	public void setLado(double lado) {
+		this.lado = lado;
+	}
+
+	// metodo toString
+	@Override
+	public String toString() {
+		return "Cuadrado [nombre=" + nombre + ", color=" + color + ", id=" + id + ", lado=" + lado + "]";
+	}
+
+	
+	
+	
+}
